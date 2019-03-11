9ed599de581010a48dc8d39df5530dfd40114b82
yongshuang <yongshuang@gdgyun.com>
2019-03-11 18:10:47 +0800

    add slide

---
+++ src/components/ebook/EbookSlide.vue
@@ -0,0 +1,51 @@
+<template>
+  <transition name="fade-slide-right">
+    <div>
+      <div class="slide-content-wrapper" v-show="menuVisible && settingVisible===3">
+        <div class="content">
+          <div class="content-page-wrapper">
+            <div class="content-page"></div>
+            <div class="content-page-tab"></div>
+          </div>
+        </div>
+        <div class="content-bg"></div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { ebookMixin } from "../../utils/mixin";
+export default {
+  mixins: [ebookMixin]
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/styles/global.scss";
+.slide-content-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 300;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  .content {
+    flex: 0 0 85%;
+    width: 85%;
+    height: 100%;
+    .content-page-wrapper {
+      .content-page {
+      }
+      .content-page-tab {
+      }
+    }
+  }
+  .content-bg {
+    flex: 0 0 15%;
+    width: 15%;
+    height: 100%;
+  }
+}
+</style>
