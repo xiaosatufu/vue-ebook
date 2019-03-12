241da88707406fc0c150d510ed8600678b58f786
yongshuang <yongshuang@gdgyun.com>
2019-03-12 17:41:44 +0800

    loading

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -4,7 +4,7 @@
     <div class="slide-content-wrapper" v-show="menuVisible && settingVisible===3">
       <transition name="slide-right">
         <div class="content" v-if="settingVisible===3">
-          <div class="content-page-wrapper">
+          <div class="content-page-wrapper" v-if="bookAvailable">
             <div class="content-page">
               <component :is="currentTab===1?content:bookmark"></component>
             </div>
@@ -19,6 +19,9 @@
               >{{$t('book.bookmark')}}</div>
             </div>
           </div>
+          <div class="content-empty" v-else>
+            <ebook-loading></ebook-loading>
+          </div>
         </div>
       </transition>
       <div class="content-bg" @click="hideTitleAndMenu()"></div>
@@ -30,10 +33,12 @@
 <script>
 import { ebookMixin } from "../../utils/mixin";
 import EbookSlideContents from "./EbookSlideContents"
+import EbookLoading from "./EbookLoading"
 export default {
   mixins: [ebookMixin],
   components: {
-    EbookSlideContents
+    EbookSlideContents,
+    EbookLoading
   },
   data(){
     return {
@@ -85,6 +90,11 @@ export default {
           @include center;
         }
       }
+    }
+    .content-empty {
+      width: 100%;
+      height: 100%;
+      @include center;
     }
   }
   .content-bg {
